9f6dd04aacfc4f3e161d4399ff55b390c87c18e5
Adam The Canadian <adam.jarrett.clare@gmail.com>
2022-11-24 09:49:24 -0700

    Adding new double based types for 3x3 matrix and 3x1 vector

---
+++ include/cmat/mat3d.h
@@ -0,0 +1,8 @@
+#ifndef CMAT_INCLUDE_CMAT_MAT3D_H_
+#define CMAT_INCLUDE_CMAT_MAT3D_H_
+
+typedef struct Matrix3x3Double {
+  double mat[9];
+} Mat3d;
+
+#endif
---
+++ include/cmat/vec3d.h
@@ -0,0 +1,15 @@
+#ifndef CMAT_INCLUDE_CMAT_VEC3D_H_
+#define CMAT_INCLUDE_CMAT_VEC3D_H_
+
+typedef struct Vector3Double{
+  union {
+    struct {
+      double x;
+      double y;
+      double z;
+    };
+    double vec[3];
+  };
+} Vec3fd;
+
+#endif

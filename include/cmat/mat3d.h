a56e7e46bbe7ae5050c7ee155a8f2c594ba23ab1
Adam The Canadian <adam.jarrett.clare@gmail.com>
2022-11-25 08:18:24 -0700

    Fix naming of mat3d

--- include/cmat/mat3d.h
+++ include/cmat/mat3d.h
@@ -13,26 +13,26 @@ void Mat3d_SetZero(Mat3d *mat);
 /**
  * \return The column vector (col) from a matrix (mat)
  */
-Vec3d Vec3d_GetCol(const Vec3d *mat,
+Vec3d Mat3d_GetCol(const Mat3d *mat,
                    int col);
 
 /**
  * \return The row vector (row) from a matrix (mat)
  */
-Vec3d Vec3d_GetRow(const Vec3d *mat,
+Vec3d Mat3d_GetRow(const Mat3d *mat,
                    int row);
 
 /**
  * Set the (row) of matrix (mat) from the vector (rowVec)
  */
-void Vec3d_SetRow(Vec3d *mat,
+void Mat3d_SetRow(Mat3d *mat,
                   const Vec3d *rowVec,
                   int row);
 
 /**
  * Set the (col) of matrix (mat) from the vector (colVec)
  */
-void Vec3d_SetCol(Vec3d *mat,
+void Mat3d_SetCol(Mat3d *mat,
                   const Vec3d *colVec,
                   int col);
 #endif
